3d6914b78606cbd625f287c403b4bef046e0e828
BART WITSCHEY FELIX DIAZ <bart.witschey@atos.net>
2024-01-12 15:50:25 -0600

    Initial V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -5,5 +5,30 @@ provider "aws" {
 resource "aws_instance" "demo-server" {
   ami = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
-  key_name = "dpp" 
+  key_name = "dpp"
+  security_groups = aws_security_group.allow_tls
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
