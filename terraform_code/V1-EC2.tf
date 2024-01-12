49da8c97a3eb89a03fe49d742207c68cc2d87a86
BART WITSCHEY FELIX DIAZ <bart.witschey@atos.net>
2024-01-12 17:23:10 -0600

    V1-EC2.tf with additional EC2s and VPC

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -6,12 +6,18 @@ resource "aws_instance" "demo-server" {
   ami = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   key_name = "dpp"
-  security_groups = aws_security_group.allow_tls
+  vpc_security_group_ids = aws_security_group.allow_tls.id
+  subnet_id = aws_subnet.dpw-public_subent_01.id
+  for_each = toset(["jenkins-master", "jenkins-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
 }
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id = aws_vpc.dpw-vpc.id
 
   tags = {
     Name = "allow_tls"
@@ -32,3 +38,57 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+ resource "aws_vpc" "dpw-vpc" {
+       cidr_block = "10.1.0.0/16"
+       tags = {
+        Name = "dpw-vpc"
+     }
+   }
+
+resource "aws_subnet" "dpw-public_subent_01" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_01"
+    }
+}
+
+resource "aws_subnet" "dpw-public_subent_02" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+    tags = {
+      Name = "dpw-public_subent_02"
+    }
+}
+
+resource "aws_internet_gateway" "dpw-igw" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    tags = {
+      Name = "dpw-igw"
+    }
+}
+
+resource "aws_route_table" "dpw-public-rt" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpw-igw.id
+    }
+    tags = {
+      Name = "dpw-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-1" {
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-2" {
+    subnet_id = aws_subnet.dpw-public_subent_02.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
